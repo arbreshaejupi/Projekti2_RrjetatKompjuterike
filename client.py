c2d3c9e1f269de4478454d0ecf79cfd208825ff1
ArbreshaHodolli <94081430+ArbreshaHodolli@users.noreply.github.com>
2022-04-11 17:29:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -6,3 +6,9 @@ ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+    connected = True
