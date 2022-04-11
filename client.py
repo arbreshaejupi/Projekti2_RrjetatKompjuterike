b002f3cf63fe919e4336bb8afc23797085ea109a
ArbreshaHodolli <94081430+ArbreshaHodolli@users.noreply.github.com>
2022-04-11 22:33:32 +0200

    Update client.py

--- client.py
+++ client.py
@@ -34,7 +34,7 @@ def file_type(type):
             f.write("Now the file has more content!")
             f.close()
 
-            # open and read the file after the appending:
+           
             f = open("write.txt", "r")
             print(f.read())
 
